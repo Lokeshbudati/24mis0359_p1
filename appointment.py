1c8052c45143069a4be71e27488cd0352aac8ff2
Lokeshbudati <budatilokesh05@gmail.com>
2026-07-19 11:27:43 +0000

    Final project

--- appointment.py
+++ appointment.py
@@ -4,3 +4,5 @@ def register_patient():
     print("Patient Registration Module")
 def login():
     print("User Login Successful")
+print("Appointment booked from the main branch")
+print("Appointment booked from conflict branch")
