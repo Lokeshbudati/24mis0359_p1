dda50072240ed49dc3d51602fccce9c1fbe07997
Lokeshbudati <budatilokesh05@gmail.com>
2026-07-19 11:31:30 +0000

    Updated appointment.py

--- appointment.py
+++ appointment.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 print("Hospital Appointment Booking System")
 
 def register_patient():
@@ -7,6 +7,4 @@ def login():
     print("User Login Successful")
 print("Appointment booked from the main branch")
 print("Appointment booked from conflict branch")
-=======
-print("Hospital Management Booking System")
->>>>>>> cf93fb21b93c3f2a8304331c149bacf91dc58b61
+
